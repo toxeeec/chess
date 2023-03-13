ae5fd4d9a658f59a6ddfb22c56df898c0e8c525e
toxeeec <bartosz.kapciak@gmail.com>
2023-03-13 02:15:06 +0100

    feat: add frontend

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import "./globals.css";
+
+export default function Home() {
+	return (
+		<div className="flex justify-center items-center h-full prose max-w-none">
+			<h1>Hello World</h1>
+		</div>
+	);
+}
