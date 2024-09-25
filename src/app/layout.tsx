83ee492f3a6fc6b2a370e057a793cb884af0cc0a
toxeeec <bartosz.kapciak@gmail.com>
2024-09-25 21:45:51 +0200

    feat: add title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
 import "./globals.css"
 
+export const metadata = {
+	title: "chess",
+}
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
